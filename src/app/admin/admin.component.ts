81de92a94140ee9d8d62d517afed602c49f3f283
Anekoui <a.nekoui@workingspirit.nl>
2019-07-22 13:43:21 +0200

    bezig met trajectupdate

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -3,6 +3,7 @@ import { JwtToken, AutenticatieService } from '../services/autenticatie.service'
 import { TrajectService } from '../services/traject.service';
 import { Router } from '@angular/router';
 import { Traject } from '../traject';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'wsa-admin',
@@ -11,17 +12,31 @@ import { Traject } from '../traject';
 })
 export class AdminComponent implements OnInit {
   gebruiker: JwtToken;
-
   traject: Traject = new Traject();
+
+  alleTrajecten : Traject[];
 
   constructor(private trajectService: TrajectService, 
     private router: Router, private authenticatieService:AutenticatieService) { }
 
   ngOnInit() {
     this.haalGebruikerOp();
+    this.getAlleTrajecten();
   }
 
   haalGebruikerOp():void {
     this.gebruiker = this.authenticatieService.haalTokenOp();
   }
+
+  
+  getAlleTrajecten(): void {
+    this.trajectService.GeefAlleTrajecten()
+        .subscribe(alleTrajecten => this.alleTrajecten = alleTrajecten);
+  }
+
+  updateTraject(traject:Traject){
+    this.trajectService.WijzigTraject(traject)
+    .subscribe(response => this.router.navigateByUrl('/admin'));
+  }
+
 }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,14 +8,12 @@ import {StudentComponent} from "./student/student.component";
 import {DocentComponent} from "./docent/docent.component";
 import {DocentAutorisatieGuard} from "./guards/docent-autorisatie.guard";
 import {StudentAutorisatieGuard} from "./guards/student-autorisatie.guard";
-import { AdminMaakTrajectComponent } from './admin/admin-maak-traject/admin-maak-traject.component';
 
 const routes: Routes = [
 
   {path: 'home', component: HomeComponent},
   {path: 'login', component: LoginComponent},
   {path: 'admin', component: AdminComponent, canActivate: [AdminAutorisatieGuard]},
-  {path: 'admin-maak-traject', component: AdminMaakTrajectComponent, canActivate: [AdminAutorisatieGuard]},
   {path: 'student', component: StudentComponent, canActivate: [StudentAutorisatieGuard]},
   {path: 'docent', component: DocentComponent, canActivate: [DocentAutorisatieGuard]},
   {path: '', pathMatch: 'full', redirectTo: '/login',}
