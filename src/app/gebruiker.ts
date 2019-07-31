49be2f5999982706c83da87c362a3ff59cb06252
Marco Kerkdijk <m.kerkdijk@gmail.com>
2019-07-31 13:24:50 +0200

    Opstarten webserver voor Azure

--- src/app/gebruiker.ts
+++ src/app/gebruiker.ts
@@ -1,11 +1,12 @@
-import { Rol } from './rol.enum';
+import {Rol} from './rol.enum';
 
 export class Gebruiker {
-    id: number;
-    emailadres: string;
-    voornaam: string;
-    tussenvoegsel: string;
-    achternaam: string;
-    rol: Rol;
-    actief: boolean;
+  id: number;
+  wachtwoord: string;
+  emailadres: string;
+  voornaam: string;
+  tussenvoegsel: string;
+  achternaam: string;
+  rol: Rol;
+  actief: boolean;
 }
