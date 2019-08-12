193c301d20fb9b7d4427d032b4ecd5b400321ed9
KikkertJ <49674106+KikkertJ@users.noreply.github.com>
2019-08-12 09:48:21 +0200

    Update docent-opdrachten.component.spec.ts

--- src/app/docent/docent-opdrachten/docent-opdrachten.component.spec.ts
+++ src/app/docent/docent-opdrachten/docent-opdrachten.component.spec.ts
@@ -1,5 +1,4 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { DocentOpdrachtenComponent } from './docent-opdrachten.component';
 
 describe('DocentOpdrachtenComponent', () => {
