564581e2a2bba0e2c6cdd932e1e866042751275e
WietseGr <w.graafmans@workingspirit.nl>
2019-08-26 14:08:46 +0200

    traject informatie + agenda worden getoont op student homepagina

--- src/app/student/student-home/student-home.component.ts
+++ src/app/student/student-home/student-home.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { AutenticatieService, JwtToken } from 'src/app/services/autenticatie.service';
 import { Router } from '@angular/router';
+import { Gebruiker } from 'src/app/Objecten/gebruiker';
+import { Traject } from 'src/app/Objecten/traject';
+import { AutenticatieService, JwtToken } from 'src/app/services/autenticatie.service';
+import { TrajectService } from 'src/app/services/traject.service';
+import {Observable, of} from 'rxjs';
+import { SelectieFase } from 'src/app/Objecten/selectieFase';
+import { OpleidingsFase } from 'src/app/Objecten/opleidingsfase';
 
 @Component({
   selector: 'wsa-student-home',
@@ -9,17 +15,32 @@ import { Router } from '@angular/router';
 })
 export class StudentHomeComponent implements OnInit {
   gebruiker:JwtToken;
+  index: number = 0;
+  trajecten: Traject[];
+  SelectieFase: SelectieFase[]; 
+  OpleidingsFase: OpleidingsFase[]; 
+
+  
   
   constructor(
     private authenticatieService:AutenticatieService,
-    private router: Router,) { }
+    private router: Router,
+    private trajectService: TrajectService,
+    ) { }
 
   ngOnInit() {
     this.haalGebruikerOp();
+    this.HaalTrajectBijGebruikerOp();
   }
   
   haalGebruikerOp():void {
     this.gebruiker = this.authenticatieService.haalTokenOp();
+  }
+
+  HaalTrajectBijGebruikerOp(){
+    this.trajectService.haalTrajectenOpVanGebruiker(this.gebruiker.gebruiker_id)
+    .subscribe(trajecten => this.trajecten = trajecten);
+ 
   }
 
 }
