8deb22c48623cc7489dd7a741c2a0629690997e0
Frankkeijzer <49674171+Frankkeijzer@users.noreply.github.com>
2019-08-29 13:35:46 +0200

    Update assessment-tabel.component.spec.ts

--- src/app/admin/admin-beheer-assessments/assessment-tabel/assessment-tabel.component.spec.ts
+++ src/app/admin/admin-beheer-assessments/assessment-tabel/assessment-tabel.component.spec.ts
@@ -1,5 +1,4 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { AssessmentTabelComponent } from './assessment-tabel.component';
 
 describe('AssessmentTabelComponent', () => {
