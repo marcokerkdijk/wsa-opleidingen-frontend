6583385083bcc31151c112e8644989c18a6acf67
Frankkeijzer <f.keijzer@workingspirit.nl>
2019-08-30 10:00:47 +0200

    Voortgang in het opschonen van Admin-Traject

--- src/app/admin/admin-beheer-traject/admin-beheer-traject.component.spec.ts
+++ src/app/admin/admin-beheer-traject/admin-beheer-traject.component.spec.ts
@@ -1,5 +1,4 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { AdminBeheerTrajectComponent } from './admin-beheer-traject.component';
 
 describe('AdminBeheerTrajectComponent', () => {
