8802cdce64bc84a930815984f04802d6a8475981
Anekoui <a.nekoui@workingspirit.nl>
2019-08-30 12:03:25 +0200

    admin kan resultaten zien

--- src/app/services/uitwerking.service.ts
+++ src/app/services/uitwerking.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { Observable } from 'rxjs';
 import { Uitwerking } from '../Objecten/uitwerking';
@@ -12,7 +12,7 @@ export class UitwerkingService {
 
   constructor(private http: HttpClient) { }
 
-  haalAlleUitwerkingenOp(){
+  haalAlleUitwerkingenOp() {
     return this.http.get<Uitwerking[]>(`${this.api}/geefAlleUitwerkingen`)
   }
 
