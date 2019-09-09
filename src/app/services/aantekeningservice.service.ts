a99e61c9f97187ea2b1b6a181ec94cba27806490
Frankkeijzer <49674171+Frankkeijzer@users.noreply.github.com>
2019-09-09 13:42:33 +0200

    Update aantekeningservice.service.ts

--- src/app/services/aantekeningservice.service.ts
+++ src/app/services/aantekeningservice.service.ts
@@ -4,7 +4,6 @@ import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { HttpClient } from '@angular/common/http';
 
-
 @Injectable({
   providedIn: 'root'
 })
