b5cefe6030f043cd827d1cf462855a72e1048afa
Frankkeijzer <f.keijzer@workingspirit.nl>
2019-09-26 12:31:45 +0200

    Toevoegen van gebruikers gewijzigd, modal opgeruimd

--- src/app/admin/admin-beheer-gebruikers/beheer-gebruikers.component.ts
+++ src/app/admin/admin-beheer-gebruikers/beheer-gebruikers.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { environment } from 'src/environments/environment';
 import { GebruikersService } from 'src/app/services/gebruikers.service';
 import { Gebruiker } from 'src/app/Objecten/gebruiker';
-import { ModalService } from 'src/app/services/modal.service';
 import { Router } from '@angular/router';
 
 @Component({
@@ -13,14 +11,11 @@ import { Router } from '@angular/router';
 export class BeheerGebruikersComponent implements OnInit {
   gebruikerInvoer: Gebruiker = new Gebruiker;
   actieveGebruikers : Gebruiker[];
-  private api:string = environment.apiUrl;
-  bodyText:string;
-
+  // bodyText:string;
  
-  constructor(private router: Router, private modalService: ModalService, private gebruikerService : GebruikersService) { }
+  constructor(private router: Router, private gebruikerService : GebruikersService) { }
  
   ngOnInit() {
-    this.bodyText = 'This text can be updated in modal 1';
     this.haalActieveGebruikers();
   }
   
@@ -28,24 +23,9 @@ export class BeheerGebruikersComponent implements OnInit {
     this.gebruikerService.geefActieveGebruikers()
         .subscribe(gebruikers => this.actieveGebruikers = gebruikers);
   }
-     
-    openModal(id) {
-      this.modalService.open(id);
-  }
-
-  sluitModal(id) {
-      this.modalService.close(id);
-  }
-  
+ 
   haalGebruikerOpId(id:number): void {
     this.gebruikerService.vraagGebruikerOpId(id).subscribe(gebruiker => this.gebruikerInvoer = gebruiker);
   }
 
-  voegGebruikerToe(gebruiker:Gebruiker) {
-    this.gebruikerService.gebruikerToevoegen(gebruiker)
-    .subscribe(response => this.router.navigateByUrl('/admin').then(Succes => { 
-      this.router.navigateByUrl('/admin/admin-beheer-gebruikers')
-    }));
-  }
 }
-
