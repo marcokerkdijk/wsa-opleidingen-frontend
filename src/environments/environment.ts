07297877e10a26a2d951103d0689be777d8afb06
KikkertJ <j.kikkert@workingspirit.nl>
2019-10-24 11:12:41 +0200

    aanpassing

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'https://wsa-opleidingen-test.azurewebsites.net',
+  apiUrl: 'https://localhost:8080',
   wsaClientId: 'wsa-opleidingen',
   wsaClientSecret: 'lancenNicaLCONiT'
 };
